7730c80063c6d1649e5a462fa7a8c9a5e98a4b91
omkanta <1omkant1998@gmail.com>
2022-09-21 18:36:46 +0530

    Prime number

---
+++ Prime.js
@@ -0,0 +1,11 @@
+    let flag="No";
+    let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i===0){
+            count++;
+        }
+    }
+    if(count<=2){
+        flag="Yes";
+    }
+    console.log(flag);
